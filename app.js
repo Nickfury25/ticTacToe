6084083853b474cbce68d0b37388c85325a54af7
Ray <nickfury25@yahoo.com>
2022-06-22 11:10:55 -0500

    first tic tac toe commit

---
+++ app.js
@@ -0,0 +1,30 @@
+let initialState;
+
+function biggerBoard(){
+
+    
+}
+
+function buildInitialState(){
+
+
+
+}
+
+function renderState(){
+
+
+
+}
+
+
+function onBoardClick(){
+
+
+
+    renderState()
+}
+
+const board = document.getElementById("tic-tac-toe-board");
+board.addEventListener('click', onBoardClick);
+
